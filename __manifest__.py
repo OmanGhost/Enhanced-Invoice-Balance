c068014501c9cbac6b8908e850d14da71ebff428
OmanGhost <38818982+OmanGhost@users.noreply.github.com>
2025-01-19 23:43:31 +0400

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom Invoice Balance',
+    'version': '17.0.1.0.5',
+    'category': 'Accounting',
+    'summary': 'Display balance in invoice form under partner name',
+    'author': 'SH HOSTING LLC',
+    'website': "https://www.sh.om",
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
